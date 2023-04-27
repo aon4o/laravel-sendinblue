45594a9ffefb801640094629166b957c16c52ae2
Aleksandar Naydenov <aleksandar.naydenov@delta.bg>
2023-04-27 14:43:32 +0300

    Enum replaced with array.

--- src/Objects/ErrorResponse.php
+++ src/Objects/ErrorResponse.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace Aon2003\LaravelSendInBlue\Objects;
 
-use Aon2003\LaravelSendInBlue\Enums\ErrorCodes;
-use ReflectionEnum;
-use ReflectionException;
-
 /**
  * Class for formulating errors returned by the package.
  */
@@ -17,6 +13,21 @@ class ErrorResponse
     public string $messageCode;
     public string $message;
     public string $reason;
+
+    protected array $errorCodes = [
+        'invalid_parameter' => 'The value of the parameter you have provided is not valid. Please check the format and the type',
+        'missing_parameter' => 'One of the required parameter is missing to perform the request',
+        'out_of_range' => 'The value of the parameter you have provided is not included in the authorized range',
+        'unauthorized' => 'You are not authorized to do this call',
+        'reseller_permission_denied' => 'You need a reseller plan to perform this API call',
+        'document_not_found' => 'The parameter value in brackets {} is not found',
+        'method_not_allowed' => 'The method you are requesting for this path is not allowed. (ex : you are doing put but only get method is allowed for the path',
+        'not_enough_credits' => 'You don\'t have enough credit to perform the request. Example : you are trying to send a campaign but your plan has expired',
+        'duplicate_parameter' => 'You have duplicated one of the parameter in the request',
+        'duplicate_request' => 'The request rate of the very same request is too high',
+        'account_under_validation' => 'Your account is under validation',
+        'permission_denied' => 'You don\'t have the permission to perform this request',
+    ];
 
     /**
      * @param int $code
@@ -31,19 +42,6 @@ class ErrorResponse
         $this->messageCode = $array_response->code;
         $this->reason = $array_response->message;
 
-        $this->message = $this->getMessageText($this->messageCode);
-    }
-
-    /**
-     * @param string $message_code
-     * @return string
-     */
-    protected function getMessageText(string $message_code): string
-    {
-        try {
-            return (new ReflectionEnum(ErrorCodes::class))->getCase($message_code)->getValue()->value;
-        } catch (ReflectionException) {
-            return '';
-        }
+        $this->message = $this->errorCodes[$this->messageCode] ?? '';
     }
 }
